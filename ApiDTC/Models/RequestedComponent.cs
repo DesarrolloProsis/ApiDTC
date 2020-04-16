5a0e7911caf50f0114f12093ae2acfa520070dd7
suripanta <axel.frias257@gmail.com>
2020-04-16 13:58:34 -0500

    Solución manual

--- ApiDTC/Models/RequestedComponent.cs
+++ ApiDTC/Models/RequestedComponent.cs
@@ -14,23 +14,23 @@
         
         public string Marca { get; set; }
         
-        public string[] Modelo { get; set; }
+        public string Modelo { get; set; }
         
         public string NumSerie { get; set; }
         
         public string Unity { get; set; }
         
-        public DateTime[] DateInstallationDate { get; set; }
+        public DateTime DateInstallationDate { get; set; }
         
-        public DateTime[] DateMaintenanceDate { get; set; }
+        public DateTime DateMaintenanceDate { get; set; }
 
         public string MaintenanceFolio { get; set; }
 
-        public int[] IntLifeTimeExpected { get; set; }
+        public int IntLifeTimeExpected { get; set; }
         
         public DateTime DateLifeTimeReal { get; set; }
         
-        public int[] IntPartida { get; set; }
+        public int IntPartida { get; set; }
         #endregion
     }
 }
