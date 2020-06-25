e5fa2b531ae3f2b804c53cee2b757977482e8ad0
Axel Frías <axel.frias257@gmail.com>
2020-06-25 13:07:02 -0500

    Standard models

---
+++ ApiDTC/Models/ComponentTableForm.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiDTC.Models
+{
+    public class ComponentTableForm
+    {
+        public string Componente { get; set; }
+        public string Cantidad { get; set; }
+        public string Lane { get; set; }
+
+    }
+}
--- ApiDTC/Models/DtcDataStr.cs
+++ ApiDTC/Models/DtcDataStr.cs
@@ -3,42 +3,41 @@
     using System;
     using System.ComponentModel.DataAnnotations;
 
-    public class DtcData
+    public class DtcDataStr
     {
         #region Properties
 
-        [Required]        
+        [Required]
         public string ReferenceNumber { get; set; }
-        
+
         public string SinisterNumber { get; set; }
-        
+
         public string ReportNumber { get; set; }
-        
+
         public DateTime SinisterDate { get; set; }
-        
+
         public DateTime FailureDate { get; set; }
-        
+
         public string FailureNumber { get; set; }
-        
+
         public DateTime ShippingDate { get; set; }
-        
+
         public DateTime ElaborationDate { get; set; }
-        
+
+
         public string Observation { get; set; }
-        
+
         public string Diagnosis { get; set; }
-        
-        public int UserId { get; set; }
-        
+
+        public string TypeDescription { get; set; }
+
+        public string StatusDescription { get; set; }
+
+        public int StatusId { get; set; }
+
         public int TypeDescriptionId { get; set; }
-        
-        public int AgremmentInfoId { get; set; }
 
-        public int DTCStatus { get; set; }
 
-        public bool flag { get; set; }
         #endregion
     }
-
-    
 }
