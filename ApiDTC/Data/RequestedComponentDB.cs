7aabf9b48f03c3830aa95c0e6be3a3255156e6c9
suripanta <axel.frias257@gmail.com>
2020-09-03 11:30:43 -0500

    Prueba inicial modo libre

--- ApiDTC/Data/RequestedComponentDB.cs
+++ ApiDTC/Data/RequestedComponentDB.cs
@@ -32,6 +32,7 @@
 
                 foreach (var item in requestedComponent)
                 {
+                    /* MODO NO LIBRE
                     SqlCommand cmd = new SqlCommand("dbo.sp_InsertComponents", sql);
                     cmd.CommandType = CommandType.StoredProcedure;
 
@@ -52,7 +53,7 @@
                     cmd.Parameters.Add("@strLifeTimeReal", SqlDbType.NVarChar).Value = item.strLifeTimeReal;
                     cmd.Parameters.Add("@intPartida", SqlDbType.Int).Value = item.IntPartida;
                     cmd.Parameters.Add("@strMaintenanceFolio", SqlDbType.NVarChar).Value = item.MaintenanceFolio;
-
+                    
                     if(item == requestedComponent[requestedComponent.Count - 1])
                         cmd.Parameters.Add("@validationFlag", SqlDbType.Bit).Value = 1;
                     else
@@ -62,10 +63,30 @@
                         cmd.Parameters.Add("@startFlag", SqlDbType.Bit).Value = 1;
                     else
                         cmd.Parameters.Add("@startFlag", SqlDbType.Bit).Value = 0;
+                    */
+                    SqlCommand cmd = new SqlCommand("dbo.spInsertComponentsOpen", sql);
+                    cmd.CommandType = CommandType.StoredProcedure;
 
+                    cmd.Parameters.Add("@intType", SqlDbType.Int).Value = 1;
+                    cmd.Parameters.Add("@bitflag", SqlDbType.Bit).Value = flag;
+                    cmd.Parameters.Add("@strReferenceNumber", SqlDbType.NVarChar).Value = item.StrReferenceNumber;
+                    cmd.Parameters.Add("@strUnity", SqlDbType.NVarChar).Value = item.StrUnity;
+                    cmd.Parameters.Add("@strComponent", SqlDbType.NVarChar).Value = item.StrComponent;
+                    cmd.Parameters.Add("@strBrand", SqlDbType.NChar).Value = item.StrBrand;
+                    cmd.Parameters.Add("@strModel", SqlDbType.NVarChar).Value = item.StrModel;
+                    cmd.Parameters.Add("@strSerialNumber", SqlDbType.NVarChar).Value = item.StrSerialNumber;
+                    cmd.Parameters.Add("@strLane", SqlDbType.NVarChar).Value = item.StrLane;
+                    cmd.Parameters.Add("@dateInstallationDate", SqlDbType.DateTime).Value = item.DateInstallationDate;
+                    cmd.Parameters.Add("@dateMaintenanceDate", SqlDbType.DateTime).Value = item.DateMaintenanceDate;
+                    cmd.Parameters.Add("@strLifeTimeExpected", SqlDbType.NVarChar).Value = item.StrLifeTimeExpected;
+                    cmd.Parameters.Add("@intItem", SqlDbType.Int).Value = item.IntItem;
+                    cmd.Parameters.Add("@strMaintenanceFolio", SqlDbType.NVarChar).Value = item.StrMaintenanceFolio;
+                    cmd.Parameters.Add("@strlifeTimeReal", SqlDbType.NVarChar).Value = item.StrLifeTimeReal;
+                    cmd.Parameters.Add("@strUnitaryPrice", SqlDbType.NVarChar).Value = item.StrUnitaryPrice;
+                    cmd.Parameters.Add("@strDollarUnitaryPrice", SqlDbType.NVarChar).Value = item.StrDollarUnitaryPrice;
+                    cmd.Parameters.Add("@strTotalPrice", SqlDbType.NVarChar).Value = item.StrTotalPrice;
+                    cmd.Parameters.Add("@srDollarTotalPrice", SqlDbType.NVarChar).Value = item.StrDollarTotalPrice;
                     result = _sqlResult.Post(cmd, sql);
-
-
                     if (result.SqlResult == null)
                     {
                         result.SqlMessage = $"{result.SqlMessage}. No se pudo insertar la partida del modelo {item.Modelo} tipo 1.";
@@ -74,7 +95,7 @@
                 }
                 foreach (var item in requestedComponent)
                 {
-                    SqlCommand cmd = new SqlCommand("dbo.sp_InsertComponents", sql);
+                    /*SqlCommand cmd = new SqlCommand("dbo.sp_InsertComponents", sql);
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.Parameters.Add("@intType", SqlDbType.Int).Value = 2;
                     cmd.Parameters.Add("@flag", SqlDbType.Int).Value = flag;
@@ -106,7 +127,30 @@
                         cmd.Parameters.Add("@startFlag", SqlDbType.Bit).Value = 1;
                     else
                         cmd.Parameters.Add("@startFlag", SqlDbType.Bit).Value = 0;
+                    */
+                    SqlCommand cmd = new SqlCommand("dbo.spInsertComponentsOpen", sql);
+                    cmd.CommandType = CommandType.StoredProcedure;
 
+                    cmd.Parameters.Add("@intType", SqlDbType.Int).Value = 2;
+                    cmd.Parameters.Add("@bitflag", SqlDbType.Bit).Value = flag;
+                    cmd.Parameters.Add("@strReferenceNumber", SqlDbType.NVarChar).Value = item.StrReferenceNumber;
+                    cmd.Parameters.Add("@strUnity", SqlDbType.NVarChar).Value = item.StrUnity;
+                    cmd.Parameters.Add("@strComponent", SqlDbType.NVarChar).Value = item.StrComponent;
+                    cmd.Parameters.Add("@strBrand", SqlDbType.NChar).Value = item.StrBrand;
+                    cmd.Parameters.Add("@strModel", SqlDbType.NVarChar).Value = item.StrModel;
+                    cmd.Parameters.Add("@strSerialNumber", SqlDbType.NVarChar).Value = item.StrSerialNumber;
+                    cmd.Parameters.Add("@strLane", SqlDbType.NVarChar).Value = item.StrLane;
+                    cmd.Parameters.Add("@dateInstallationDate", SqlDbType.DateTime).Value = item.DateInstallationDate;
+                    cmd.Parameters.Add("@dateMaintenanceDate", SqlDbType.DateTime).Value = item.DateMaintenanceDate;
+                    cmd.Parameters.Add("@strLifeTimeExpected", SqlDbType.NVarChar).Value = item.StrLifeTimeExpected;
+                    cmd.Parameters.Add("@intItem", SqlDbType.Int).Value = item.IntItem;
+                    cmd.Parameters.Add("@strMaintenanceFolio", SqlDbType.NVarChar).Value = item.StrMaintenanceFolio;
+                    cmd.Parameters.Add("@strlifeTimeReal", SqlDbType.NVarChar).Value = item.StrLifeTimeReal;
+                    cmd.Parameters.Add("@strUnitaryPrice", SqlDbType.NVarChar).Value = item.StrUnitaryPrice;
+                    cmd.Parameters.Add("@strDollarUnitaryPrice", SqlDbType.NVarChar).Value = item.StrDollarUnitaryPrice;
+                    cmd.Parameters.Add("@strTotalPrice", SqlDbType.NVarChar).Value = item.StrTotalPrice;
+                    cmd.Parameters.Add("@srDollarTotalPrice", SqlDbType.NVarChar).Value = item.StrDollarTotalPrice;
+                    result = _sqlResult.Post(cmd, sql);
                     result = _sqlResult.Post(cmd, sql);
                     if (result.SqlResult == null)
                     {
