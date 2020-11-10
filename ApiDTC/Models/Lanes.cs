2f22df70fc0f441ca3715e63db976aa9c2759b78
Axel Frías <axel.frias257@gmail.com>
2020-11-10 16:39:05 -0600

    SpSquareLanes

---
+++ ApiDTC/Models/Lanes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDTC.Models
+{
+    public class Lanes
+    {
+        public string Lane { get; set; }
+
+        public int IdGare { get; set; }
+
+        public int CapufeLaneNum { get; set; }
+    }
+}
