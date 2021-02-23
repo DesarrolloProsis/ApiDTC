5ed7856bbcc03fc9aa243cd576cc0b3bd11d9127
Machinne <ramr16@outlook.com>
2021-02-23 11:48:50 -0600

    LoginTrue into master remote

---
+++ ApiDTC/Models/LoginTrue.cs
@@ -0,0 +1,19 @@
+namespace ApiDTC.Models
+{
+    using System;
+
+    public class LoginTrue
+    {
+        #region Properties
+        public int UserId { get; set; }
+        
+        public string SquareCatalogId { get; set; }
+
+        public int RollId { get; set; }
+
+        public string SquareName { get; set; }
+
+        public int AdminSquareId { get; set; }
+        #endregion
+    }
+}
