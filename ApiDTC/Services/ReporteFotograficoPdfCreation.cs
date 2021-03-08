0a8e6f05e3ce24a0d162bc2821eb944b97f54598
LANUBE <38117134+DesarrolloProsis@users.noreply.github.com>
2021-03-08 09:59:26 -0600

    Publicación corrección de error en hora y fecha mantenimientoÂ

--- ApiDTC/Services/ReporteFotograficoPdfCreation.cs
+++ ApiDTC/Services/ReporteFotograficoPdfCreation.cs
@@ -383,7 +383,7 @@
                 //TODO poner la hora inicio del stored de Alex en reporte fotográfico equipo nuevo y dañado
                 string valorHoraInicio = _tipo == 1 ? Convert.ToString(_tableHeader.Rows[0]["Inicio"]) : "12:00";
                 var inicioDateTime = Convert.ToDateTime(valorHoraInicio);
-                string conversionInicio = inicioDateTime.ToString("hh:mm:ss tt", CultureInfo.CurrentCulture);
+                string conversionInicio = inicioDateTime.ToString("hh:mm tt", CultureInfo.CurrentCulture);
                 var colTextoHoraInicio = new PdfPCell(new Phrase("Hora INICIO: ", letraoNegritaChica)) { Border = 0, HorizontalAlignment = Element.ALIGN_RIGHT, VerticalAlignment = Element.ALIGN_CENTER, Padding = 4 };
                 var colHoraInicio = new PdfPCell(new Phrase(conversionInicio, letraNormalChica)) { BorderWidthBottom = 1, BorderWidthTop = 0, BorderWidthLeft = 0, BorderWidthRight = 0, HorizontalAlignment = Element.ALIGN_CENTER, VerticalAlignment = Element.ALIGN_BOTTOM, Padding = 2 };
 
@@ -404,8 +404,8 @@
                 table.AddCell(ubicacion);
                 CeldasVacias(2, table);
                 string valorHoraFin = _tipo == 1 ? Convert.ToString(_tableHeader.Rows[0]["Fin"]) : "13:00";
-                var finDateTime = Convert.ToDateTime(valorHoraInicio);
-                string conversionFin = inicioDateTime.ToString("hh:mm:ss tt", CultureInfo.CurrentCulture);
+                var finDateTime = Convert.ToDateTime(valorHoraFin);
+                string conversionFin = finDateTime.ToString("hh:mm tt", CultureInfo.CurrentCulture);
                 var colTextoHoraFin = new PdfPCell(new Phrase("Hora FIN: ", letraoNegritaChica)) { Border = 0, HorizontalAlignment = Element.ALIGN_RIGHT, VerticalAlignment = Element.ALIGN_CENTER, Padding = 4 };
                 var colHoraFin = new PdfPCell(new Phrase(conversionFin, letraNormalChica)) { BorderWidthBottom = 1, BorderWidthTop = 0, BorderWidthLeft = 0, BorderWidthRight = 0, HorizontalAlignment = Element.ALIGN_CENTER, VerticalAlignment = Element.ALIGN_BOTTOM, Padding = 2 };
 
