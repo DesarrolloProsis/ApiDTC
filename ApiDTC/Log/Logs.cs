737c4811aa6812d506968922ef589827e68130fe
Carlos Molina <moec17@gmail.com>
2022-01-31 14:51:56 -0600

    Segunda prueba del gitignore

---
+++ ApiDTC/Log/Logs.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDTC.Log
+{
+    public class Logs
+    {
+        public void CreateFile(object obj, Exception ex)
+        {
+            string fecha = System.DateTime.Now.ToString("yyyyMMdd");
+            string hora = System.DateTime.Now.ToString("HH:mm:ss");
+            string path = @"D:\Logs\" + fecha + ".txt";
+
+            StreamWriter sw = new StreamWriter(path, true);
+
+            StackTrace stacktrace = new StackTrace();
+            sw.WriteLine(obj.GetType().FullName + " " + hora);
+            sw.WriteLine(stacktrace.GetFrame(1).GetMethod().Name + " - " + ex.Message);
+            sw.WriteLine("");
+
+            sw.Flush();
+            sw.Close();
+        }
+
+        public void CreateFileNoError(object obj, string message)
+        {
+            string fecha = System.DateTime.Now.ToString("yyyyMMdd");
+            string hora = System.DateTime.Now.ToString("HH:mm:ss");
+            string path = @"D:\Logs\" + "Mensajes" + ".txt";
+
+            StreamWriter sw = new StreamWriter(path, true);
+
+            StackTrace stacktrace = new StackTrace();
+            sw.WriteLine(obj.GetType().FullName + " " + hora);
+            sw.WriteLine(stacktrace.GetFrame(1).GetMethod().Name + " - " + message);
+            sw.WriteLine("");
+
+            sw.Flush();
+            sw.Close();
+        }
+
+        public void CreateFileReference(object obj, string message, string reference)
+        {
+            string fecha = System.DateTime.Now.ToString("yyyyMMdd");
+            string hora = System.DateTime.Now.ToString("HH:mm:ss");
+            string path = @"D:\Logs\" + reference + ".txt";
+
+            StreamWriter sw = new StreamWriter(path, true);
+
+            StackTrace stacktrace = new StackTrace();
+            sw.WriteLine(obj.GetType().FullName + " " + hora);
+            sw.WriteLine(stacktrace.GetFrame(1).GetMethod().Name + " - " + message);
+            sw.WriteLine("");
+
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
