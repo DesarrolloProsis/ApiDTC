2ac0889a62bf9a57f846bd4ef30035c10eb89578
LaMeraChuleta <geoffreyytorres25@gmail.com>
2022-02-02 06:29:22 -0600

    home

---
+++ ApiDTC/Controllers/AnexoDTCController.cs
@@ -0,0 +1,35 @@
+﻿using ApiDTC.Data;
+using ApiDTC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDTC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnexoDTCController : ControllerBase
+    {
+        private readonly AnexoDtcDb _db;
+        public AnexoDTCController(AnexoDtcDb db)
+        {
+            this._db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+        
+        [HttpGet("{clavePlaza}/{referenceNumber}")]
+        public ActionResult<Response> GetComponent(string clavePlaza, string referenceNumber)
+        {
+            if (ModelState.IsValid)
+            {
+                var get = _db.GetComponentAnexo(clavePlaza, referenceNumber);
+                if (get.Result == null)
+                    return BadRequest(get);
+                else
+                    return Ok(get);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ ApiDTC/Data/AnexoDtcDb.cs
@@ -0,0 +1,56 @@
+﻿using ApiDTC.Models;
+using ApiDTC.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDTC.Data
+{
+    public class AnexoDtcDb
+    {
+        #region Attributes
+        private readonly string _connectionString;
+
+        private readonly SqlResult _sqlResult;
+
+        private readonly ApiLogger _apiLogger;
+        #endregion
+
+        #region Constructor
+        public AnexoDtcDb(IConfiguration configuration, SqlResult sqlResult, ApiLogger apiLogger)
+        {
+            _sqlResult = sqlResult;
+            _apiLogger = apiLogger;
+            _connectionString = configuration.GetConnectionString("defaultConnection");
+        }
+        #endregion
+
+        public Response GetComponentAnexo(string clavePlaza, string referenceNumber)
+        {
+            try
+            {
+                using (SqlConnection sql = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("dbo.GetComponentesAnexo", sql))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("@referenceNumber", SqlDbType.NVarChar).Value = referenceNumber;
+                        var componentes = _sqlResult.GetList<ComponentesAnexoValidos>(clavePlaza, cmd, sql, "GetComponentesAnexo");
+
+                        return componentes;
+                    }
+
+                }             
+            }
+            catch (SqlException ex)
+            {
+                _apiLogger.WriteLog(clavePlaza, ex, "CalendarioDb: InsertComment", 1);
+                return new Response { Message = $"Error: {ex.Message}", Result = null };
+            }
+        }
+    }
+}
