3bd9eafb63cdfb0a6be0b9e54d9d1632193b6027
Carlos Molina <moec17@gmail.com>
2022-04-27 16:25:32 -0500

    Cambio al PDf del DTC

--- ApiDTC/Services/PageEventHelperDtc.cs
+++ ApiDTC/Services/PageEventHelperDtc.cs
@@ -392,7 +392,7 @@ namespace ApiDTC.Services
             var col29 = new PdfPCell(new Phrase("", letraNormalChica)) { Border = 0 };
             var col30 = new PdfPCell(new Phrase("", letraNormalChica)) { Border = 0 };
 
-            string labelCordinacion = "Coordinación Regional";
+            string labelCordinacion = "Unidad Regional";
             if (_newConvenio.ConvenioNuevo) 
             {
                 labelCordinacion = "Unidad Regional";
@@ -411,7 +411,7 @@ namespace ApiDTC.Services
             var col37 = new PdfPCell(new Phrase("", letraNormalChica)) { Border = 0 };
             var col38 = new PdfPCell(new Phrase("", letraNormalChica)) { Border = 0 };
 
-            var col39 = new PdfPCell(new Phrase("Centro de Servicio", letraNormalChica)) { Border = 0 };
+            var col39 = new PdfPCell(new Phrase("", letraNormalChica)) { Border = 0 };
             var col40 = new PdfPCell(new Phrase("", letraoNegritaChicaEncabezado)) { HorizontalAlignment = Element.ALIGN_RIGHT, Border = 0 };
 
             var col41 = new PdfPCell(new Phrase("", letraNormalChica)) { Border = 0 };
